b9f34946b329721d30a72a70d443e5091ea40f48
LAZERED_0 <antonkmeta2005anna@gmail.com>
2022-11-14 14:19:47 +0300

    continuation of work

---
+++ PZ_6/PZ_6_2.py
@@ -0,0 +1,29 @@
+"""
+Дан список размера N. Найти количество участков, на которых его элементы
+монотонно возрастают.
+"""
+
+import random
+def program():
+    try:
+        lst = [random.randint(0, 1000) for el in range(int(input('Введите размер списка: ')))]
+        print(lst)
+        result = 0
+        count = 0
+        for j in range(len(lst) - 2):
+            if lst[j + 2] > lst[j + 1] > lst[j]:
+                count += 1
+            elif count >= 1 and lst[j + 1] > lst[j + 2]:
+                result += 1
+                count = 0
+        if lst[-1] > lst[-2] > lst[-3]:
+            result += 1
+        print(result)
+
+    except ValueError:
+        print("Ошибка ввода")  # Оповещание об ошибке
+        program()  # Повторный вызов функции из-за ошибки
+
+
+program()
+
